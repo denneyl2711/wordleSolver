30836941765ea5e8ca7b2f63fa17fcdd93b9a577
denneyl2711 <lukedenneymn@gmail.com>
2024-04-05 16:38:42 -0500

    Added a static function to create a state diagram which checks if a letter is at a particular index.

--- Dfa.py
+++ Dfa.py
@@ -39,18 +39,97 @@ class Dfa:
 
         ]
         
-        letter_index = ord(letter) - ord('a')
-        stateDiagram[0][letter_index] = 1
+        letter_as_index = ord(letter) - ord('a')
+        stateDiagram[0][letter_as_index] = 1
         
+        #note: accepting state must be 0 in this dfa
+        return stateDiagram
+    
+    def create_diag_with_letter_index(letter, index):
+        stateDiagram = [
+              #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z         
+              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #state 0
+              [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], #state 1
+              [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], #state 2
+              [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4], #state 3
+              [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], #state 4
+              [5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5], #state 5 (perpetual accepting state)
+              [6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6]  #state 6 (trap state)
+        
+        ]
+        
+        letter_as_index = ord(letter) - ord('a')
+        
+        #rows before the index stay the same
+        #(each state simply leads to the next state)
+
+        #first row after the index:
+            #if it's the target letter, go to the accepting state
+            #if it's not the target letter, go to the trap state
+
+        for k in range(26):
+            if k == letter_as_index:
+                stateDiagram[index][k] = 5 #point to accepting state
+            else:
+                stateDiagram[index][k] = 6 #point to trap state
+                
+        #we only have to change one row
+        #the rest of the rows (besides accepting and trap) represent states which cannot be accessed
+
+        
+        #note: accepting state must be 5 in this dfa
         return stateDiagram
 
 
-        #example: no 'a'
-        # stateDiagram = [
-        #          #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z     
-        #          [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #state 0
-        #          [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  #trap state
 
-        # ]
-        # acceptingState = 0
+
+
+ ############################################################################################################################
+    #example DFA tables
+    #need to create support for NFAs?
+
+    #has an 'a'
+    # stateDiagram = [
+    #     #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z         
+    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #state 0
+    #     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  #state 1
+    # ]
+    # acceptingState = 1
+
+    #has an 'a' as a third character
+    # stateDiagram = [
+    #           #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z         
+    #           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #state 0
+    #           [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], #state 1
+    #           [3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4], #state 2
+    #           [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], #state 3 (perpetual accepting state)
+    #           [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]  #state 4 (trap state)
+        
+    # ]
+    # acceptingState = 3
+
+    #DOES NOT have an 'a' as the third character
+    #note: this is the same as the previous state diagram, but the 3's and 4's in state 2 are swapped
+    # stateDiagram = [
+    #            #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
+    #            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #state 0
+    #            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ,2, 2], #state 1
+    #            [4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], #state 2
+    #            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], #state 3 (perpetual accepting state)
+    #            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]  #state 4 (trap state)
+               
+
+    # ]
+    # acceptingState = 3
+
+
+
+    #no 'a' at all
+    # stateDiagram = [
+    #              #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z     
+    #              [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #state 0
+    #              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  #trap state
+
+    # ]
+    # acceptingState = 0
     
--- wordleSolver.py
+++ wordleSolver.py
@@ -20,61 +20,24 @@ def main():
     #calculate better first two guesses later?
     guesses = ["stair", "bound", "", "", "", ""]
     
-    ###############################################
-    #example DFA tables
-    #need to create support for NFAs?
-
-    #has an 'a'
-    # stateDiagram = [
-    #     #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z         
-    #     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #state 0
-    #     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  #state 1
-    # ]
-    # acceptingState = 1
-
-    #has an 'a' as a third character
-    # stateDiagram = [
-    #           #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z         
-    #           [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #state 0
-    #           [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2], #state 1
-    #           [3, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4], #state 2
-    #           [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], #state 3 (perpetual accepting state)
-    #           [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]  #state 4 (trap state)
-        
-    # ]
-    # acceptingState = 3
-
-    #DOES NOT have an 'a' as the third character
-    #note: this is the same as the previous state diagram, but the 3's and 4's in state 2 are swapped
-    # stateDiagram = [
-    #            #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z
-    #            [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1], #state 0
-    #            [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2 ,2, 2], #state 1
-    #            [4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], #state 2
-    #            [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3], #state 3 (perpetual accepting state)
-    #            [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]  #state 4 (trap state)
-               
-
-    # ]
-    # acceptingState = 3
-
-    #no 'a' at all
-    stateDiagram = [
-                 #a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z     
-                 [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0], #state 0
-                 [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  #trap state
-
-    ]
-    acceptingState = 0
     
-    count = 0
-    max_words = 10
     #test_dfa = Dfa(stateDiagram, acceptingState)
-    diagram_without_letter = Dfa.create_diag_without_letter('l')
+    diagram_without_letter = Dfa.create_diag_without_letter('b')
     test_dfa = Dfa(diagram_without_letter, 0) #accepting state is 0 for diagrams which only check if a letter is present
     
+    test_dfa_2 = Dfa(Dfa.create_diag_with_letter_index('c', 0), 5)
+
+    count = 0
+    max_words = 1000
     for word in wordle_answer_words:
-        print(word + " is accepted: " + str(test_dfa.is_string_accepted(word)))
+        #print(word + " is accepted: " + str(test_dfa.is_string_accepted(word)))
+
+        # if (test_dfa_2.is_string_accepted(word)):
+        #     print(word + " is accepted: " + str(test_dfa_2.is_string_accepted(word)))
+
+        if test_dfa.is_string_accepted(word) and test_dfa_2.is_string_accepted(word):
+            print(word + " is accepted")
+            
         count = count + 1
         if count >= max_words:
             break;
