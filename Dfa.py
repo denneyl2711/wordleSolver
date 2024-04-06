6459666eff0b1139383aa111812490f2d38d98a2
denneyl2711 <lukedenneymn@gmail.com>
2024-04-05 22:32:23 -0500

    Remembered to finish committing changes.

--- Dfa.py
+++ Dfa.py
@@ -1,6 +1,7 @@
 class Dfa:
     state_diagram = None
     accepting_state = -1
+    #assume the start state is always 0
     
     def __init__(self, stateDiagram, acceptingState):
         self.state_diagram = stateDiagram
@@ -12,9 +13,20 @@ class Dfa:
     def get_accepting_state(self):
         return self.accepting_state
     
+    def __repr__(self):
+        output = ""
+        output = output + " a  b  c  d  e  f  g  h  i  j  k  l  m  n  o  p  q  r  s  t  u  v  w  x  y  z" + '\n'
+        i = 0
+        for row in self.state_diagram:
+            output = output + str(row) + " state " + str(i) + "\n"
+            i = i + 1
+        output = output + "Accepting state: " + str(self.accepting_state)
+        
+        return output
+
+    
     def is_string_accepted(self, inputString):
         startState = 0
-        #acceptingState = 1 #TODO: make a class to encapsulate the state diagram transitions and the accepting state(s)
 
         while inputString:
             currentState = startState
@@ -23,6 +35,7 @@ class Dfa:
                 integer_value = ord(char) - ord('a')
                 #print(integer_value, char)
                 currentState = self.state_diagram[currentState][integer_value]
+                    
            
     
             if currentState == self.accepting_state:
@@ -41,7 +54,7 @@ class Dfa:
         
         letter_as_index = ord(letter) - ord('a')
         stateDiagram[0][letter_as_index] = 1
-        
+                
         #note: accepting state must be 0 in this dfa
         return stateDiagram
     
@@ -74,14 +87,54 @@ class Dfa:
             else:
                 stateDiagram[index][k] = 6 #point to trap state
                 
+                
         #we only have to change one row
         #the rest of the rows (besides accepting and trap) represent states which cannot be accessed
 
         
         #note: accepting state must be 5 in this dfa
         return stateDiagram
+    
+    #TODO: DOES NOT PRODUCT CORRECT OUTPUT
+    def create_dfa_intersection(dfa1, dfa2):
+        stateDiagram1 = dfa1.get_state_diagram()
+        stateDiagram2 = dfa2.get_state_diagram()
+        
+        #number of states in final diagram is, at most, stateDiagram1.length * stateDiagram2.length
+        # print("First dfa has " + str(len(stateDiagram1)) + " states")
+        # print("Second dfa has " + str(len(stateDiagram2)) + " states")
+        intersection_diagram = [[]for i in range(len(stateDiagram1) * len(stateDiagram2))]
 
+        
+        #TODO: optimize so that each DFA knows its alphabet and we can go through fewer states?
+        alphabet = "abcdefghijklmnopqrstuvwxyz"
+        
+        state = 0
+        for i in range(len(stateDiagram1)):
+            for k in range(len(stateDiagram2)):
+                for letter in alphabet:
+                    letter_as_index = ord(letter) - ord('a')
+                    calculated_state = 0
+                    
+                    #where does state diagram 1 go?
+                    calculated_state = calculated_state + stateDiagram1[i][letter_as_index]
+                    
+                    #where does state diagram 2 go?
+                    calculated_state = calculated_state + stateDiagram2[k][letter_as_index]
+                    
+                    state_index = i * len(stateDiagram2) + k
+                    intersection_diagram[state_index].append(calculated_state)
+                    
+                    
+                    
+                state = state + 1
 
+        a_state_1 = dfa1.get_accepting_state()
+        a_state_2 = dfa2.get_accepting_state()
+        
+        accepting_state = a_state_1 * len(stateDiagram2) + a_state_2
+        
+        return Dfa(intersection_diagram, accepting_state)
 
 
 
--- wordleSolver.py
+++ wordleSolver.py
@@ -1,3 +1,4 @@
+from re import L
 from turtle import write_docstringdict
 from Dfa import Dfa
 
@@ -22,25 +23,39 @@ def main():
     
     
     #test_dfa = Dfa(stateDiagram, acceptingState)
-    diagram_without_letter = Dfa.create_diag_without_letter('b')
-    test_dfa = Dfa(diagram_without_letter, 0) #accepting state is 0 for diagrams which only check if a letter is present
+    diagram_without_letter = Dfa.create_diag_without_letter('l')
+    test_dfa = Dfa(diagram_without_letter, 0)  # accepting state is 0 for diagrams which only check if a letter is present
+
+    diagram_letter_index = Dfa.create_diag_with_letter_index('g', 3)
+    test_dfa_2 = Dfa(diagram_letter_index, 5)
+
+    #test_dfa_2 = Dfa(Dfa.create_diag_without_letter('a'), 0)
     
-    test_dfa_2 = Dfa(Dfa.create_diag_with_letter_index('c', 0), 5)
+    intersection_dfa = Dfa.create_dfa_intersection(test_dfa, test_dfa_2)
+    
+    print(intersection_dfa)
 
     count = 0
-    max_words = 1000
+    max_matches = 10
     for word in wordle_answer_words:
         #print(word + " is accepted: " + str(test_dfa.is_string_accepted(word)))
 
         # if (test_dfa_2.is_string_accepted(word)):
         #     print(word + " is accepted: " + str(test_dfa_2.is_string_accepted(word)))
 
-        if test_dfa.is_string_accepted(word) and test_dfa_2.is_string_accepted(word):
+        # if test_dfa.is_string_accepted(word) and test_dfa_2.is_string_accepted(word):
+        #     print(word + " is accepted")
+
+        if intersection_dfa.is_string_accepted(word):
             print(word + " is accepted")
+           
             
-        count = count + 1
-        if count >= max_words:
-            break;
+            
+        if test_dfa.is_string_accepted(word) and test_dfa_2.is_string_accepted(word):
+            print("            " + word + " accepted by manual intersection")
+            count = count + 1
+            if count >= max_matches:
+                break
     
     
 
